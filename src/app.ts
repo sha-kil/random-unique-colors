163558e870f73b30fe1bb56c794afd880daa39b3
ShakilAhmedDhaka <gk.shakil@gmail.com>
2022-04-08 22:04:54 -0400

    n random colors

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("hello colors!");
+export function getNColors(n: number) {
+  const goldenAngle = 180 * (3 - Math.sqrt(5));
+  let colors = [];
+
+  for (let i = 0; i < n; i++) {
+    colors.push(`hsl(${i * goldenAngle}, 100%, 70%)`);
+  }
+
+  return colors;
+}
